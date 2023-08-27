6f949fced1300171887dc9704cc472cf84cf40be
themultiplexer <joshua.rutschmann@gmx.de>
2023-08-27 23:04:04 +0200

    Better scanning and errors

--- bibbot.py
+++ bibbot.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.firefox.options import Options
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 from urllib.parse import urlparse, parse_qs
 
 hostName = "localhost"
-serverPort = 8080
+serverPort = 8281
 
 options = webdriver.FirefoxOptions()
 options.add_argument("--headless")
@@ -16,7 +17,76 @@ driver = webdriver.Firefox(options=options)
 driver.install_addon("./bibbotmod.xpi")
 driver.install_addon("./cookies.xpi")
 
+sites = ["https://www.spiegel.de",
+        "https://www.zeit.de",
+        "https://www.wiwo.de",
+        "https://www.welt.de",
+        "https://www.tagesspiegel.de",
+        "https://www.sueddeutsche.de",
+        "https://sz-magazin.sueddeutsche.de",
+        "https://www.handelsblatt.com",
+        "https://www.berliner-zeitung.de",
+        "https://www.morgenpost.de",
+        "https://www.nordkurier.de",
+        "https://www.abendblatt.de",
+        "https://www.moz.de",
+        "https://www.noz.de",
+        "https://www.waz.de",
+        "https://www.heise.de",
+        "https://www.maz-online.de",
+        "https://www.lr-online.de",
+        "https://www.nachrichten.at",
+        "https://ga.de",
+        "https://www.ksta.de",
+        "https://www.rundschau-online.de",
+        "https://rp-online.de",
+        "https://www.tagesanzeiger.ch",
+        "https://www.falter.at",
+        "https://www.stuttgarter-zeitung.de",
+        "https://www.stuttgarter-nachrichten.de",
+        "https://www.ostsee-zeitung.de",
+        "https://www.stimme.de",
+        "https://kurier.at",
+        "https://freizeit.at",
+        "https://www.diepresse.com",
+        "https://www.sn.at",
+        "https://www.kleinezeitung.at",
+        "https://www.vn.at",
+        "https://www.thueringer-allgemeine.de",
+        "https://www.mopo.de",
+        "https://www.saechsische.de",
+        "https://www.freiepresse.de",
+        "https://www.haz.de",
+        "https://www.lvz.de",
+        "https://www.dnn.de",
+        "https://www.swp.de",
+        "https://www.ruhrnachrichten.de",
+        "https://www.businessinsider.de",
+        "https://www.badische-zeitung.de",
+        "https://www.stern.de",
+        "https://www.mittelbayerische.de",
+        "https://www.tagblatt.de",
+        "https://www.mz.de",
+        "https://www.capital.de",
+        "https://www.iz.de",
+        "https://www.shz.de",
+        "https://www.aerztezeitung.de",
+        "https://www.geo.de",
+        "https://www.nzz.ch",
+        "https://www.manager-magazin.de"
+      ]
+
 class MyServer(BaseHTTPRequestHandler):
+
+    def error(self, text):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>BibBot Server</title></head>", "utf-8"))
+        self.wfile.write(bytes("<h2>%s</h2>" % text, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+    
     def do_GET(self):
         query_components = parse_qs(urlparse(self.path).query)
 
@@ -25,32 +95,32 @@ class MyServer(BaseHTTPRequestHandler):
             url = query_components["url"][0]
 
         if self.path.startswith("/get") and url is not None:
+            print("Checking...", url)
+            if not url.startswith(tuple(sites)):
+                self.error("BibBot does not support this page yet :/")
+                return
             
-
+            print("Loading...", url)
             driver.get("https://google.com")
             driver.get(url)
-            #driver.execute_script("window.localStorage.setItem('key','value');")
-            #options = driver.execute_script("return window.localStorage.getItem('providerOptions');")
-            #print(options)
 
-            #presence_of_element_located Web
+            print("Waiting for Bibbot extension to load")
+
             if url != "https://google.com":
-                found = False
-                while not found:
-                    found = True
-                    try:
-                        driver.find_element(By.XPATH, "//*[@id='bibbot-loader']")
-                    except NoSuchElementException:
-                        found = False
+                
+                try:
+                    WebDriverWait(driver, 5.0).until(ec.presence_of_element_located((By.XPATH, "//*[@id='bibbot-loader']")))
+                except TimeoutException:
+                    self.error("BibBot Extension not initilizaing. (Timeout)")
+                    return
 
                 print("Found loader")
 
-                while found:
-                    found = True
-                    try:
-                        driver.find_element(By.XPATH, "//*[@id='bibbot-loader']")
-                    except NoSuchElementException:
-                        found = False
+                try:
+                    WebDriverWait(driver, 15.0).until(ec.invisibility_of_element_located((By.XPATH, "//*[@id='bibbot-loader']")))
+                except TimeoutException:
+                    self.error("BibBot Extension did not load article. (Timeout)")
+                    return
 
                 print("Loaded article")
 
